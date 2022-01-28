b5b43462d80139cb5d5cdbbf62893d9a29c36ddb
Johannes Herforth <johannes.herforth@rwth-aachen.de>
2022-01-28 17:20:46 +0100

    added some event point features - obj type counts - obj created in event - OHE activity existence

--- examples/event_example.py
+++ examples/event_example.py
@@ -4,10 +4,12 @@ from ocel_features.obj.event_point import Event_Based
 
 def main():
     a = Event_Based(ocel.import_log('logs/actual-min.jsonocel'))
-    a.add_relation_created_count()
-
+    # a.add_relation_created_count()
+    # a.add_obj_type_counts()
+    # a.add_new_obj_created_counts()
+    a.add_activity_OHE()
     print(a.df_full().columns)
-    print(a.df_values())
+    print(a.df_full().values)
 
 
 if __name__ == '__main__':
--- ocel_features/obj/event_point.py
+++ ocel_features/obj/event_point.py
@@ -1,6 +1,7 @@
 import inspect
 import pandas as pd
 import numpy as np
+import ocel_features.util.ocel_helper as oh
 from ocel_features.util.multigraph import create_object_centric_graph, \
     Relations, _RELATION_DELIMITER
 
@@ -50,6 +51,86 @@ class Event_Based:
                             event_index = self._ev_index[e]
                             col_values[event_index][rel_index[relk]] += 1
                             col_values[event_index][rel_index['TOTAL']] += 1
+
+        # add to df
+        self._op_log.append(para_log)
+        self._df[col_name] = col_values
+
+    def add_obj_type_counts(self):
+        # control
+        para_log = (func_name(), )
+        if para_log in self._df.columns:
+            print(f'[!] {para_log} already computed. Skipping..')
+            return
+
+        # df setup
+        events = self._log['ocel:events']
+        objects = self._log['ocel:objects']
+        obj_types = self._log['ocel:global-log']['ocel:object-types']
+        ot_index = {ot: i for i, ot in enumerate(obj_types)}
+        ot_index['TOTAL'] = len(obj_types)
+        row_count = len(self._df.index)
+        col_name = [f'{_FEATURE_PREFIX}otype_{ot}_count' for ot in obj_types]
+        col_name.append(f'{_FEATURE_PREFIX}otype_TOTAL_count')
+        col_values = np.zeros((row_count, len(col_name)), dtype=np.uint64)
+
+        for e, vals in events.items():
+            event_index = self._ev_index[e]
+            for o in vals['ocel:omap']:
+                o_type = objects[o]['ocel:type']
+                col_values[event_index][ot_index[o_type]] += 1
+                col_values[event_index][ot_index['TOTAL']] += 1
+
+        # add to df
+        self._op_log.append(para_log)
+        self._df[col_name] = col_values
+
+    def add_new_obj_created_counts(self):
+        # control
+        para_log = (func_name(), )
+        if para_log in self._df.columns:
+            print(f'[!] {para_log} already computed. Skipping..')
+            return
+
+        # df setup
+        objects = self._graph.nodes()
+        obj_types = self._log['ocel:global-log']['ocel:object-types']
+        ot_index = {ot: i for i, ot in enumerate(obj_types)}
+        ot_index['TOTAL'] = len(obj_types)
+        row_count = len(self._df.index)
+        col_name = [f'{_FEATURE_PREFIX}obj_{ot}_created_count'
+                    for ot in obj_types]
+        col_name.append(f'{_FEATURE_PREFIX}otype_TOTAL_created_count')
+        col_values = np.zeros((row_count, len(col_name)), dtype=np.uint64)
+
+        for obj, prop in objects.items():
+            event_index = self._ev_index[prop['object_events'][0]]
+            col_values[event_index][ot_index[prop['type']]] += 1
+            col_values[event_index][ot_index['TOTAL']] += 1
+
+        # add to df
+        self._op_log.append(para_log)
+        self._df[col_name] = col_values
+
+    def add_activity_OHE(self):
+        # control
+        para_log = (func_name(), )
+        if para_log in self._df.columns:
+            print(f'[!] {para_log} already computed. Skipping..')
+            return
+
+        # df setup
+        events = self._log['ocel:events']
+        activity_names = oh.get_activity_names(self._log)
+        an_index = {ot: i for i, ot in enumerate(activity_names)}
+        row_count = len(self._df.index)
+        col_name = [f'{_FEATURE_PREFIX}activity:{an}'
+                    for an in activity_names]
+        col_values = np.zeros((row_count, len(col_name)), dtype=np.bool8)
+
+        for e, instance in events.items():
+            event_index = self._ev_index[e]
+            col_values[event_index][an_index[instance['ocel:activity']]] = 1
 
         # add to df
         self._op_log.append(para_log)
