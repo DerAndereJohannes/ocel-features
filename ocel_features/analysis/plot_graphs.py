ab01884c5ce372f293bd4da9fbd854d598cf70f8
Johannes Herforth <johannes.herforth@rwth-aachen.de>
2022-03-02 12:10:59 +0100

    added option to hide labels in graph

--- ocel_features/analysis/plot_graphs.py
+++ ocel_features/analysis/plot_graphs.py
@@ -11,19 +11,23 @@ def save_graph_graphviz(net, layout='dot', name='test_graph'):
     os.system(f'open {name}.png')
 
 
-def show_graph_plt(net):
+def show_graph_plt(net, labels=False):
     weights = [len(net.get_edge_data(u, v, []))
                + len(net.get_edge_data(v, u, [])) for u, v in net.edges()]
-    edge_labels = {}
-    for u, v in net.edges():
-        if (u, v) not in edge_labels and (v, u) not in edge_labels:
-            uv = [m.relation_shorthand(x) for x in net.get_edge_data(u, v, [])]
-            vu = [m.relation_shorthand(x) for x in net.get_edge_data(v, u, [])]
-            edge_labels[u, v] = f'{u}->{v}:{uv}, {vu}'
-
     plt.figure()
     pos = nx.nx_pydot.graphviz_layout(net)
-    nx.draw_networkx_edge_labels(net, pos, edge_labels)
-    # nx.draw(net, pos=pos, with_labels=True)
+
+    if labels:
+        edge_labels = {}
+        for u, v in net.edges():
+            if (u, v) not in edge_labels and (v, u) not in edge_labels:
+                uv = [m.relation_shorthand(x)
+                      for x in net.get_edge_data(u, v, [])]
+                vu = [m.relation_shorthand(x)
+                      for x in net.get_edge_data(v, u, [])]
+                edge_labels[u, v] = f'{u}->{v}:{uv}, {vu}'
+
+        nx.draw_networkx_edge_labels(net, pos, edge_labels)
+
     nx.draw(net, pos=pos, width=weights, with_labels=True)
     plt.show()
