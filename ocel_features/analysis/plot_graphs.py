8437127ceb8737106fb4fa94bb44e1ce24937733
Johannes Herforth <johannes.herforth@rwth-aachen.de>
2022-03-18 20:01:33 +0100

    added a bunch of extra graphs and global feature extractions - cleaned up object_situation file by removing some code - moved removed code to a new folder of old which contains code written   that could be used for other purposes

--- ocel_features/analysis/plot_graphs.py
+++ ocel_features/analysis/plot_graphs.py
@@ -4,18 +4,22 @@ import matplotlib.pyplot as plt
 import ocel_features.util.multigraph as m
 
 
-def save_graph_graphviz(net, layout='dot', name='test_graph'):
+def save_graph_graphviz(net, layout='dot', name='test_graph', labels=False):
     ag = nx.nx_agraph.to_agraph(net)
     ag.layout(layout)
+    if labels:
+        edge_labels = {e: net.get_edge_data(*e) for e in net.edges()}
+        nx.draw_networkx_edge_labels(net, edge_labels)
     ag.draw(f'{name}.png')
     os.system(f'xdg-open {name}.png')
 
 
 def show_graph_plt(net, labels=False):
-    weights = [len(net.get_edge_data(u, v, []))
-               + len(net.get_edge_data(v, u, [])) for u, v in net.edges()]
+    # weights = [len(net.get_edge_data(u, v, []))
+    #            + len(net.get_edge_data(v, u, [])) for u, v in net.edges()]
     plt.figure()
-    pos = nx.nx_pydot.graphviz_layout(net)
+    # pos = nx.nx_pydot.graphviz_layout(net)
+    pos = nx.spring_layout(net)
 
     if labels:
         edge_labels = {}
@@ -28,8 +32,7 @@ def show_graph_plt(net, labels=False):
                 edge_labels[u, v] = f'{u}->{v}:{uv}, {vu}'
 
         nx.draw_networkx_edge_labels(net, pos, edge_labels)
-
-    nx.draw(net, pos=pos, width=weights, with_labels=False)
+    nx.draw(net, pos=pos, with_labels=True)
     plt.show()
 
 
@@ -37,3 +40,7 @@ def basic_graphviz(net):
     plt.figure()
     nx.draw(net, with_labels=True)
     plt.show()
+
+
+def without_isolates(net):
+    pass
---
+++ ocel_features/old/object_situation.py
@@ -0,0 +1,377 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+from copy import copy
+from networkx.algorithms.shortest_paths import shortest_path, \
+    all_shortest_paths
+from ocel_features.util.multigraph import relations_to_relnames, \
+    rel_subgraph, Relations
+from ocel_features.util.local_helper import obj_relationship_localities
+import ocel_features.obj.object_point as op
+import ocel_features.obj.object_global as og
+
+
+class Object_Situation:
+    pass
+
+
+class Event_Situation:
+    pass
+
+
+class Object_Situation_Locality_OT:
+    def __init__(self, log, graph, localities, oid, situation):
+        self._log = log
+        self._graph = graph
+        # self._log, self._graph = oh.create_subproblem(log, graph,
+        #                                               situation['objects'],
+        #                                               situation['events'])
+        self._localities = localities  # only of the included nodes
+        self._source = situation['situation_source']
+        self._target = situation['situation_target']
+        self._target_event = situation['situation_event']
+        self._situation = situation  # one one situation
+
+        self._df = pd.DataFrame()
+
+    def get_obj_attributes(self):
+        for o in self._situation['objects']:
+            for k, v in self._log['ocel:objects'][o]['ocel:ovmap'].items():
+                ot = self._log['ocel:objects'][o]['ocel:type']
+                key = f'att:{ot}:{o}:{k}'
+                self._df[key] = [v]
+
+    def get_ot_attributes(self, ot: set = None):
+        if not isinstance(ot, set):
+            ot = set(self._log['ocel:global-log']['ocel:object-types'])
+        for o in self._situation['objects']:
+            o_ot = self._log['ocel:objects'][o]['ocel:type']
+            if o_ot in ot:
+                for k, v in self._log['ocel:objects'][o]['ocel:ovmap'].items():
+                    key = f'att:{ot}:{o}:{k}'
+                    self._df[key] = [v]
+
+    def get_agg_ot_attributes(self, ot: set = None):
+        # only goes over values
+        if not isinstance(ot, set):
+            ot = set(self._log['ocel:global-log']['ocel:object-types'])
+        curr_col = copy(self._df.columns)
+        for o in self._situation['objects']:
+            o_ot = self._log['ocel:objects'][o]['ocel:type']
+            if o_ot in ot:
+                for k, v in self._log['ocel:objects'][o]['ocel:ovmap'].items():
+                    ot = self._log['ocel:objects'][o]['ocel:type']
+                    key = f'agg:{ot}:{k}'
+                    if key in curr_col:
+                        continue  # if columns already exists
+                    if isinstance(v, (int, float)):
+                        if key in self._df:
+                            self._df[key] += v
+                        else:
+                            self._df[key] = [v]
+
+    def get_event_attributes(self):
+        for e in self._situation['events']:
+            for k, v in self._log['ocel:events'][e]['ocel:vmap'].items():
+                an = self._log['ocel:events'][e]['ocel:activity']
+                key = f'att:{an}:{e}:{k}'
+                self._df[key] = [v]
+
+    def get_agg_activity_attributes(self):
+        # only goes over values
+        curr_col = copy(self._df.columns)
+        for e in self._situation['events']:
+            an = self._log['ocel:events'][e]['ocel:activity']
+            for k, v in self._log['ocel:events'][e]['ocel:vmap'].items():
+                key = f'agg:{an}:{k}'
+                if key in curr_col:
+                    continue  # if columns already exists
+                if isinstance(v, (int, float)):
+                    if key in self._df:
+                        self._df[key] += v
+                    else:
+                        self._df[key] = [v]
+
+    def get_object_point(self, fname, finput, oids):
+
+        point = op.Object_Based(self._log, self._graph, oids)
+        func = getattr(point, fname)
+
+        func(*finput)
+
+        df_dict = point.df_full().to_dict(orient='records')
+        for row in df_dict:
+            roid = row['oid']
+            for k, v in row.items():
+                if op._FEATURE_PREFIX in k:
+                    self._df[f'{roid}:{k}'] = [v]
+
+    def get_object_global(self, fname, finput, oids):
+
+        point = og.Object_Global(self._log, self._graph)
+        func = getattr(point, fname)
+
+        func(*finput)
+
+        df_dict = point.df_full().to_dict(orient='records')[0]
+
+        for k, v in df_dict.items():
+            if og._FEATURE_PREFIX in k:
+                self._df[k] = [v]
+
+    def df_numeric(self):
+        return self._df.select_dtypes(include=np.number)
+
+
+# FILTERING TO GET GOOD SITUATION PREFIXES ######
+def filter_direct_activity_involvement(log, graph, oids: set, ans: set):
+    events = log['ocel:events']
+    obj_situations = {}
+    for o in oids:
+        od = {'oid': o, 'situations': []}
+        ev_ids = set()
+        oids = set()
+        for i, ev in enumerate(graph.nodes[o]['object_events']):
+            ev_ids.add(ev)
+            oids.update(events[ev]['ocel:omap'])
+            if events[ev]['ocel:activity'] in ans:
+                situation = {'event': ev, 'index': i,
+                             'events': copy(ev_ids), 'objects': copy(oids)}
+                od['situations'].append(situation)
+
+        if not od['situations']:
+            od = None
+
+        obj_situations[o] = od
+
+    return obj_situations
+
+
+# FILTERING TO GET GOOD SITUATION PREFIXES ######
+def filter_event_ot_involvement(log, graph, oids: set, ot: set):
+    events = log['ocel:events']
+    objects = log['ocel:objects']
+    obj_situations = {}
+    for o in oids:
+        od = {'oid': o, 'situations': []}
+        ev_ids = set()
+        oids = set()
+        for i, ev in enumerate(graph.nodes[o]['object_events']):
+            ev_ids.add(ev)
+            oids.update(events[ev]['ocel:omap'])
+            omap = events[ev]['ocel:omap']
+            for o2 in omap:
+                o2_type = objects[o2]['ocel:type']
+                if o2_type in ot:
+                    situation = {'oid': o2, 'type': o2_type,
+                                 'event': ev, 'index': i,
+                                 'events': copy(ev_ids)}
+                    objs = set()
+                    for e in ev_ids:
+                        objs |= events[e]['ocel:omap']
+                    situation['objects'] = objs
+                    od['situations'].append(situation)
+
+        if not od['situations']:
+            od = None
+
+        obj_situations[o] = od
+
+    return obj_situations
+
+
+def get_oids_specific_event_involvement(log, graph, oids, eids):
+    rtn_set = set()
+    for o in oids:
+        if eids.issubset(graph.nodes[o]['object_events']):
+            rtn_set.add(o)
+
+    return rtn_set
+
+
+def filter_locality_ot_involvement(log, graph, localities, oids, ot, rels):
+    rels = relations_to_relnames(rels)
+    obj_situations = {}
+    events = log['ocel:events']
+
+    for o in oids:
+        od = {'src': o, 'situations': []}
+        for rel in rels:
+            if localities[o][rel]:
+                o_set, o_tree = localities[o][rel]
+                for o2 in o_set:
+                    if log['ocel:objects'][o2]['ocel:type'] in ot:
+                        path = shortest_path(o_tree, o, o2)
+                        situation = {'relation': rel,
+                                     'path': path}
+                        evs = []
+                        objs = set()
+                        switch_ev = graph.nodes[o]['object_events'][0]
+                        for i, oid in enumerate(path[:-1]):
+                            oe = graph.nodes[oid]['object_events']
+                            start_index = oe.index(switch_ev)
+                            for e in oe[start_index:]:
+                                if path[i+1] in events[e]['ocel:omap']:
+                                    switch_ev = e
+                                    break
+                                else:
+                                    evs.append(e)
+                                    objs |= events[e]['ocel:omap']
+
+                        situation['objects'] = objs
+                        situation['events'] = evs
+                        situation['situation_event'] = switch_ev
+                        situation['situation_source'] = o
+                        situation['situation_target'] = o2
+                        od['situations'].append(situation)
+
+        obj_situations[o] = od
+
+    return obj_situations
+
+
+def filter_global_ot_involvement(log, graph, oids, ot, rels):
+    localities = obj_relationship_localities(graph, rels)
+    rels = relations_to_relnames(rels)
+    obj_situations = {}
+
+    for o in oids:
+        od = {'oid': o, 'situations': []}
+        for rel in rels:
+            o_set, o_tree = localities[o][rel]
+            for o2 in o_set:
+                if log['ocel:objects'][o2]['ocel:type'] == ot:
+                    situation = {'o2': o2,
+                                 'paths': all_shortest_paths(graph, o, o2)}
+                    situation['objects'] = 0
+                    situation['events'] = 0
+                    od['situations'].append(situation)
+
+    return obj_situations
+
+
+def filter_local_relation_ot_involvement(log, graph, oids, ot, rels):
+    rtn_set = set()
+    localities = obj_relationship_localities(graph, rels)
+    rels = relations_to_relnames(rels)
+
+    for o in oids:
+        for rel in rels:
+            for o2 in localities[o][rel]:
+                if log['ocel:objects'][o2]['ocel:type'] == ot:
+                    rtn_set.add(o)
+                    break
+
+    return rtn_set
+
+
+def filter_target_situations(log, graph, localities, targets, rels):
+    rels = relations_to_relnames(rels)
+    obj_situations = {}
+    events = log['ocel:events']
+
+    # setup
+    igraph = rel_subgraph(graph, rels).reverse()
+
+    for o in targets:
+        situation = {'target': o}
+        situation['target_event'] = graph.nodes[o]['object_events'][0]
+        situation['objects'] = nx.descendants(igraph, o) | {o}
+        situation['graph'] = igraph.subgraph(situation['objects'])
+        sit_events = {situation['target_event']}
+        last_time = events[situation['target_event']]['ocel:timestamp']
+
+        for o2 in situation['objects']:
+            sit_events.update({e for e in graph.nodes[o2]['object_events']
+                               if events[e]['ocel:timestamp'] <= last_time})
+        situation['events'] = sit_events
+
+        obj_situations[o] = situation
+
+    return obj_situations
+
+
+def create_situations_obj_rel_ot(log, graph, localities,
+                                 src: str, ot: str, rels: set):
+    d = []
+    src = {src} if isinstance(src, str) else src
+    ot = {ot} if isinstance(src, str) else ot
+
+    # get the individual situations
+    situations = filter_locality_ot_involvement(log, graph, localities,
+                                                src, ot, rels)
+
+    # create a df object for every situation
+    for s in situations[src.pop()]['situations']:
+        d.append(Object_Situation_Locality_OT(log, graph, localities, src, s))
+    return d
+
+
+def filter_object_situation(log, igraph, targets, rels, an=None):
+
+    obj_situations = {}
+    events = log['ocel:events']
+
+    if an:
+        for o in targets:
+            an_e = [e for e in igraph.nodes[o]['object_events']
+                    if events[e]['ocel:activity'] in an]
+
+            # check if the objects events contain the needed activity name(s)
+            if an_e:
+                situation = {'target_object': o,
+                             'target_type': igraph.nodes[o]['type']}
+                situation['target_events'] = an_e
+                situation['objects'] = nx.descendants(igraph, o)
+                situation['graph'] = igraph.subgraph(situation['objects'])
+                obj_situations[o] = situation
+    else:
+        for o in targets:
+            situation = {'target_object': o,
+                         'target_type': igraph.nodes[o]['type']}
+            situation['target_event'] = igraph.nodes[o]['object_events'][0]
+            situation['objects'] = nx.descendants(igraph, o)
+            situation['graph'] = igraph.subgraph(situation['objects'])
+            obj_situations[o] = situation
+
+    return obj_situations
+
+
+def create_event_situations(log, graph, an=None):
+    e_situations = []
+    events = log['ocel:events']
+
+    for e, e_dict in events:
+        if e_dict['ocel:activity'] in an:
+            e_sit = {'target_event': e,
+                     'target_activity': e_dict['ocel:activity']}
+
+            e_situations.append(Event_Situation(log, e_sit))
+
+    return e_situations
+
+
+def create_event_situation(e, e_dict, graph):
+    return {'target_event': e,
+            'target_activity': e_dict['ocel:activity'],
+            'objects': e_dict['ocel:omap'],
+            'graph': graph}
+
+
+def create_object_situations(log, graph, targets,
+                             rels={Relations.INTERACTS}, an=None):
+    output = []
+    targets = {targets} if isinstance(targets, str) else targets
+    rels = relations_to_relnames(rels)
+    igraph = rel_subgraph(graph, rels)
+
+    situations = filter_object_situation(log, igraph, targets, rels, an)
+
+    if an:
+        for o, s in situations:
+            for e in s['target_events']:
+                output.append(Object_Situation(log, s))
+    else:
+        output = [Object_Situation(log, s) for s in situations.values()]
+
+    return output
